b272836c6756c507299fcb23fdc7da9a4cb778ee
Mileter <90982151+Mileter@users.noreply.github.com>
2024-06-23 22:47:57 -0700

    Added kern
    This is unpolished.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,59 @@
 #include <iostream>
+#include <functional>
+#include <unordered_map>
+#include <fstream>
+#include <string>
+
 #include "kern/kern.h"
+
 using namespace std;
+
+void printHelp()
+{
+  cout << "USAGE:\n"
+       << "";
+}
+
+void autoNewline(string fileIn, string fileOut, int maxKerning)
+{
+  ifstream fin(fileIn);
+  ofstream fout(fileOut+".bak");
+  renewline(fin, fout, maxKerning);
+  fin.close();
+  fout.close();
+}
+
+unordered_map<string, function<void()>> cmds = {
+  {"--help", printHelp}
+};
+
+unordered_map<string, function<void(const string&, const string&, const int&)>> fcmds = {
+  {"ana", autoNewline},
+  {"auto-newline", autoNewline}
+};
 
 int main(int argc, char* argv[]) 
 {
-  string cmd;
+  string request;
   if(argc > 1)
-    cmd = argv[1];
-  else cerr << "The program has given up.\n";
-  cout << "You have requested `" << cmd << "`.";
-  
+    request = argv[1];
+  else
+  {
+    cerr << "Dear user, please provide a task for me to do! :-)\n"
+         << "Use help for more information.\n";
+    
+    return 0;
+  }
+
+  cout << "You have requested `" << request << "`.\n";
+
+  if(argc == 2 && cmds.find(request) != cmds.end())
+    cmds[request]();
+  else if(argc == 4 && fcmds.find(request) != cmds.end())
+    fcmds[request](argv[2], argv[3], stoi(argv[4]));
+  else
+    cerr << "Dear user, your request command `" << request << "` with " << argc - 2 << " extra request details, simply does not exist.\nPlease check your spelling again.";
+
   return 0;
 }
 
