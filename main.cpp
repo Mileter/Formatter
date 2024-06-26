7184d333907c4839da0aa9d69c4252f2ba1873cc
Mileter <90982151+Mileter@users.noreply.github.com>
2024-06-26 13:08:37 -0800

    Testing III
    It might work.

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ void autoNewline(string fileIn, string fileOut, int maxKerning)
   renewline(fin, fout, maxKerning);
   fin.close();
   fout.close();
-  rename(fileOut.c_str()+".bak", fileOut.c_str());
+  rename(fileOut+reinterpret_cast<const char*>(".bak"), fileOut.c_str());
 }
 
 unordered_map<string, function<void()>> cmds = {
