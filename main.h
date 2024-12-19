411d5775d5ad5aee886bcaef196225100af5d796
Mileter <90982151+Mileter@users.noreply.github.com>
2024-12-18 17:08:00 -0800

    Add required headers.

--- main.h
+++ main.h
@@ -3,6 +3,12 @@
 #include <unordered_map>
 #include <fstream>
 #include <string>
+#include <tuple>
+
 #include <cstdio>
+#include <cstdarg>
 
 #include "kern/kern.h"
+
+#define F_BUILD_TYPE "ALPHA"
+#define F_BUILD_VER  "0.1.0.1"
